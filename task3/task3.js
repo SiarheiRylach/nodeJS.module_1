53b21e6a0476e4b73489008be5fc477863dc4f69
Siarhei Rylach <Siarhei_Rylach@epam.com>
2017-10-05 18:31:44 +0300

    updated, start to work with xlsx

--- task3/task3.js
+++ task3/task3.js
@@ -26,4 +26,12 @@ function jsonFromDir(pathDir){
 
 Promise.resolve(jsonFromDir('C:/Users/Siarhei_Rylach/Downloads/Module 1/Module 1/Task 3')).then(data=>console.log(data));
 
+xlsx.fromBlankAsync()
+    .then(workbook => {
+        // Modify the workbook.
+        workbook.sheet("Sheet1").cell("A1").value("This is neat!");
+
+        // Write to file.
+        return workbook.toFileAsync("./out.xlsx");
+    });
 

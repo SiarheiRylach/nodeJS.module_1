d81e46a5e4aa32fe10cb9c425ae92b9f40493282
Siarhei Rylach <Siarhei_Rylach@epam.com>
2017-10-06 18:23:25 +0300

    updated, almost done to do refactor this

--- task3/task3.js
+++ task3/task3.js
@@ -5,6 +5,7 @@
 const xlsx = require('xlsx-populate');
 const fs = require('fs');
 const path = require('path');
+const currentDir = './';
 
 
 function jsonFromDir(pathDir){
@@ -23,15 +24,100 @@ function jsonFromDir(pathDir){
     }).then(()=>{return objects});
 }
 
-
-Promise.resolve(jsonFromDir('C:/Users/Siarhei_Rylach/Downloads/Module 1/Module 1/Task 3')).then(data=>console.log(data));
-
-xlsx.fromBlankAsync()
-    .then(workbook => {
-        // Modify the workbook.
-        workbook.sheet("Sheet1").cell("A1").value("This is neat!");
-
-        // Write to file.
-        return workbook.toFileAsync("./out.xlsx");
+Promise.resolve(jsonFromDir(getInputDirectory()))
+    .then(data=>{
+        let counterSheets = 1;
+            xlsx.fromBlankAsync().then(workbook => {
+                    data.forEach((jsonObj)=>{
+                        let currentSheet = workbook.addSheet('obj' + counterSheets++);
+                        writeObjIntoXlsx(jsonObj, currentSheet, 1, "A");
+                    });
+                    if(workbook.sheets().length < 2){
+                        console.log("Directory doesn't have any .json files");
+                    }else{
+                        workbook.deleteSheet(0);
+                        return workbook.toFileAsync(getOutputFile()); // Write to file
+                    }
+                });
     });
+
+function writeObjIntoXlsx(obj, currentSheet, curRow, curCol) {
+    let cRowCell = curRow;
+    currentSheet.active(true);
+    for(key in obj) {
+        if(obj.hasOwnProperty(key)){
+            let fieldJson = obj[key];
+            let cColCell = curCol;
+            currentSheet.cell(cColCell + cRowCell).value(key);
+
+            if (isObject(fieldJson)) {
+                cColCell = nextLetter(cColCell);
+                if (Array.isArray(fieldJson)) {
+                    currentSheet.cell(cColCell + ++cRowCell).value("arr");
+                    cColCell = nextLetter(cColCell);
+
+                    fieldJson.forEach((elem) => {
+                        if (isObject(elem) || Array.isArray(elem)) {
+                            currentSheet.cell(cColCell + ++cRowCell).value(key);
+                            cRowCell += writeObjIntoXlsx(elem, currentSheet, ++cRowCell, nextLetter(cColCell)) + 1;
+                        } else {
+                            currentSheet.cell(cColCell + ++cRowCell).value(elem);
+                        }
+
+                    });
+                } else {
+                    currentSheet.cell(cColCell + ++cRowCell).value("obj");
+                    cColCell = nextLetter(cColCell);
+                    for (let keyIn in fieldJson) {
+                        if (fieldJson.hasOwnProperty(keyIn)) {
+                            if (isObject(fieldJson[keyIn]) || Array.isArray(fieldJson[keyIn])) {
+                                currentSheet.cell(cColCell + ++cRowCell).value(keyIn);
+                                cRowCell += writeObjIntoXlsx(fieldJson[keyIn], currentSheet, ++cRowCell, nextLetter(cColCell)) + 1;
+                            } else {
+                                currentSheet.cell(cColCell + cRowCell).value(keyIn);
+                                currentSheet.cell(nextLetter(cColCell) + cRowCell).value(fieldJson[keyIn]);
+                                cRowCell++;
+                            }
+                        }
+
+                    }
+                }
+            } else {
+                cColCell = nextLetter(cColCell);
+                currentSheet.cell(cColCell + cRowCell).value(fieldJson);
+            }
+            cRowCell++;
+        }
+    }
+    return cRowCell - curRow;
+}
+
+function nextLetter(currentLetter) {
+    return String.fromCharCode(currentLetter.charCodeAt(0) + 1);
+}
+
+function isObject(obj) {
+    return typeof obj === "object";
+}
+
+function getInputDirectory(){
+    let args = process.argv;
+    for(let i = 2; i < args.length; ++i){
+        if(args[i] === "--inputDir"){
+            return args[i + 1];
+        }
+    }
+    return currentDir;
+}
+
+function getOutputFile(){
+    let args = process.argv;
+    for(let i = 2; i < args.length; ++i){
+        if(args[i] === "--outputDir"){
+            return args[i + 1];
+        }
+    }
+    return currentDir + 'out.xlsx';
+}
+
 

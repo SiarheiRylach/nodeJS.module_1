b544ee6c5a73c7977ed659d25cef496b74950bc5
Siarhei Rylach <s.n.rylach@gmail.com>
2017-10-08 21:49:16 +0300

    final version

--- task1/task1.js
+++ task1/task1.js
@@ -2,25 +2,47 @@
  * Created by Siarhei_Rylach on 10/4/2017.
  */
 const fs = require('fs');
+const currentDir = './';
 
 function outputEvery2Line(){
-    let input = fs.createReadStream('C:/Users/Siarhei_Rylach/Downloads/Module 1/Module 1/Task 1/text.txt');
-    let rl = require('readline').createInterface({
-        input: input,
-        terminal: false
-    });
 
-    let result = "";
-    let counter = 1;
-    rl.on('line', line => {
-        if( (counter++ % 2) === 0) result += line + " ";
-    });
+	return new Promise((resolve, reject)=>{
 
-    rl.on('close',()=>console.log(result.trim()));
+		if (!fs.existsSync(getPathInputFile())){
+			reject(new Error("File doesn't exist by this path"));
+		}else{
+			let input = fs.createReadStream(getPathInputFile());
+			let rl = require('readline').createInterface({
+		        input: input,
+		        terminal: false
+			 });
+
+		    let result = "";
+		    let counter = 1;
+		    rl.on('line', line => {
+		        if( (counter++ % 2) === 0) result += line + " ";
+		    });
+
+		    rl.on('close',()=>resolve(result));
+		}
+	});
+   
+
+    function getPathInputFile(){
+    	let args = process.argv;
+		for(let i = 2; i < args.length; ++i){
+	        if(args[i] === "--inputDir"){
+	            return args[i + 1];
+	        }
+		}
+		return currentDir + 'text.txt';
+    }
 
 }
+
 if(module.parent){
-    module.exports.outputEvery2Line = outputEvery2Line;
+    module.exports = outputEvery2Line;
 }else{
-    outputEvery2Line();
+    outputEvery2Line().then((data) => console.log(data))
+    				  .catch((err)=>console.log(err.toString()));
 }
